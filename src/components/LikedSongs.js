332e46cfe7f3b4ff58b011e1b21ee1c389665239
nishant-2212 <nishant.kumar@batterysmart.in>
2023-09-10 00:36:37 +0530

    heart button

--- src/components/LikedSongs.js
+++ src/components/LikedSongs.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LikedSongs() {
-  
+  const navigate = useNavigate();
+  useEffect(() => {
+    const a = localStorage.getItem("token");
+    if (!a) {
+      navigate("/");
+    }
+  }, []);
 
-      
-  return  (
+  return (
     <div>
- <h1 className="text-2xl text-white font-semibold tracking-tight hover:underline">
-          songs
-        </h1>
-  
-      
-
-      </div>
+      <h1 className="text-2xl text-white font-semibold tracking-tight hover:underline">
+        songs
+      </h1>
+    </div>
   );
 }
 
